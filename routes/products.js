0d38303678a61a7ac7ca5071cd8f91692da19dd3
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2025-03-14 09:03:56 +0700

    Get all and simple post products

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
-
+let productSchema = require('../schemas/product')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+    let products = await productSchema.find({});
+    res.send(products);
+});
+
+router.post('/', async function(req, res, next) {
+    let newProduct = productSchema(req.body);
+    await newProduct.save()
+    res.send(newProduct);
 });
 
 module.exports = router;
